71d29a96eb703506f508785e24a147e4da5b962a
theosavourat <theosavourat@gmail.com>
2024-11-14 20:26:21 +0100

    FIX: Correct AlbumController methods index and create

--- resources/js/Pages/Albums/Index.vue
+++ resources/js/Pages/Albums/Index.vue
@@ -1,24 +1,38 @@
 <template>
-    <div>
-        <h1>Mes Albums</h1>
-        <ul>
-            <li v-for="album in albums" :key="album.id">
-                <inertia-link :href="`/albums/${album.id}`">{{
-                    album.title
-                }}</inertia-link>
-            </li>
-        </ul>
-        <inertia-link href="/albums/create">Créer un album</inertia-link>
-    </div>
+    <Head title="Albums" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                Albums
+            </h2>
+        </template>
+        <div>
+            <h1>Mes Albums</h1>
+            <ul>
+                <li v-for="album in albums" :key="album.id">
+                    <Link :href="route('albums.show', album.id)"
+                        >{{ album.title }}
+                    </Link>
+                </li>
+            </ul>
+            <Link :href="route('albums.create')">Créer un album</Link>
+        </div>
+    </AuthenticatedLayout>
 </template>
 
 <script setup>
 import { defineProps } from 'vue';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
 
-// Déclaration des props
 const props = defineProps({
     albums: Array,
 });
+
+console.log(props.albums);
 </script>
 
 <style scoped></style>
